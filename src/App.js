b953eb23e0c5faef35946b8b91fb12cce5288a9c
yachanap <yachana7523@gmail.com>
2024-06-27 19:36:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <h1>E-COM</h1>
+        <button>Click</button>
+        <Button>bootstraButton</Button>
+    </div>
+  );
+}
+
+export default App;
